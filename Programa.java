878e690fc8d12d3abf412923c7f8bbb8671b0f12
Letícia de Abreu <abreuhleticia@gmail.com>
2025-07-09 11:31:32 -0300

    Primeiro commit do projeto Java

---
+++ Programa.java
@@ -0,0 +1,14 @@
+public class Produto{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        //Criando Retangulo
+        Retangulo retangulo1 = new Retangulo();
+
+        //Informações
+        System.out.println("Altura: ");
+        retangulo1.altura=scanner.nextDouble();
+        System.out.println("Largura: ");
+        retangulo1.largura=scanner.nextDouble();
+    }
+}
---
+++ Retangulo.java
@@ -0,0 +1,19 @@
+public class Retangulo{
+    public double altura;
+    public double largura;
+
+    public double area(){
+        return altura * largura;
+    }
+
+    public double perimetro(){
+        return 2*(altura+largura);
+    }
+
+    /*public double diagonal();
+
+        return */
+
+
+
+}
